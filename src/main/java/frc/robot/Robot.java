78b842d4f923787c7c8959a85aa70f149d6dfefd
Eric Macy <eric_macy@nonik.com>
2022-02-12 13:27:39 -0500

    Add basic framework

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,12 +1,40 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//Imports for Motor Control
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+import javax.lang.model.element.VariableElement;
+
+import com.ctre.phoenix.motorcontrol.Faults;
+import com.ctre.phoenix.motorcontrol.InvertType;
+
+//For Controller (physical, this is not the motor controllers.)
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+//For Drive
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+//For Pneumatics
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+//For Cams
+import edu.wpi.first.cameraserver.CameraServer;
+
+//
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +47,25 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+  //Drive callbacks
+  private static WPI_TalonSRX driveRF = new WPI_TalonSRX(10); 
+  private static WPI_TalonSRX driveLF = new WPI_TalonSRX(11); 
+  private static WPI_TalonSRX driveRR = new WPI_TalonSRX(12);
+  private static WPI_TalonSRX driveLR = new WPI_TalonSRX(13);
+  private static DifferentialDrive robot = new DifferentialDrive(driveLF, driveRF);
+
+  //Screw callbacks
+  private static WPI_TalonSRX rightScrew = new WPI_TalonSRX(20);
+  private static WPI_TalonSRX leftScrew = new WPI_TalonSRX(21);
+
+  //Wench callbacks
+  private static WPI_TalonSRX rightWench = new WPI_TalonSRX(30);
+  private static WPI_TalonSRX leftWench = new WPI_TalonSRX(31);
+
+  //Joysticks
+  private static Joystick controller_0 = new Joystick(0);
+  private static Joystick controller_1 = new Joystick(1);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,6 +76,35 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+  //Chooser Setup
+  CameraServer.startAutomaticCapture(0);
+  CameraServer.startAutomaticCapture(1);
+
+  //Factory default all drives
+  driveRF.configFactoryDefault();
+  driveLF.configFactoryDefault();
+  driveLR.configFactoryDefault();
+  driveRR.configFactoryDefault();
+  rightScrew.configFactoryDefault();
+  leftScrew.configFactoryDefault();
+  rightWench.configFactoryDefault();
+  leftWench.configFactoryDefault();
+
+  //setup followers
+  driveRR.follow(driveRF);
+  driveLR.follow(driveLF);
+
+  //flip values so robot moves forwardard when stick-forwardard/green LEDS
+  driveRF.setInverted(true);
+  driveLF.setInverted(false);
+  driveRR.setInverted(InvertType.FollowMaster);
+  driveLR.setInverted(InvertType.FollowMaster);
+
+  //Sensor phasing
+  driveRF.setSensorPhase(true);
+  driveLF.setSensorPhase(true);
+
   }
 
   /**
