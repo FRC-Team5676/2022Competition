a1c4f4f863173e68cb3323e898e0c908f40ced9a
Eric Macy <eric_macy@nonik.com>
2022-03-05 13:21:39 -0500

    ArmRotate Logic

--- src/main/java/frc/robot/ArmRotate.java
+++ src/main/java/frc/robot/ArmRotate.java
@@ -1,49 +1,45 @@
 package frc.robot;
 
+/* For Drive */
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 /* For Pneumatics */
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class ArmRotate {
 
+    private static WPI_TalonSRX armRotate = new WPI_TalonSRX(24);
     private static AirCylinder armLatch = new AirCylinder(0, 4, 5, PneumaticsModuleType.CTREPCM);
-    private static boolean latch = true;
+    private static boolean IsLatched = true;
 
-    ArmRotate() {
-
-    }
-
-    /* Arm Latch */
-    boolean latchChange = false;
-    boolean latchButton = ctl1.ButtonY();
-    if (latchButton) {
-      latch = !latch;
-      latchChange = true;
-    }
-    if (latchChange) {
-      if (latch) {
+    public static void Latch() {
+        IsLatched = true;
         armLatch.Extend(false);
         armRotate.set(-0.5);
         armLatch.Extend(true);
         armRotate.stopMotor();
-      } else {
-        armLatch.Extend(false);
-      }
-      latchChange = false;
     }
 
-        /* Arm Rotate */
-        double rotSpeed = ctl1.RightStickY() * ctl1.RightStickY();
-        if (armLatch.IsExtended())
-          rotSpeed = 0.0;
-        if (ctl1.RightStickY() > 0.0) {
-          armRotate.set(rotSpeed);
-        } else if (ctl1.RightStickY() < 0.0) {
-          armRotate.set(-rotSpeed);
-        } else {
-          armRotate.set(0);
+    public static void Unlatch() {
+        IsLatched = false;
+        armLatch.Extend(false);
+    }
+
+    public static void RotateUp(double speed) {
+        if (IsLatched)
+            armRotate.stopMotor();
+        else {
+            speed = Math.abs(speed);
+            armRotate.set(speed);
         }
-    
-    
+    }
+
+    public static void RotateDown(double speed) {
+        if (IsLatched)
+            armRotate.stopMotor();
+        else {
+            speed = Math.abs(speed);
+            armRotate.set(-speed);
+        }
+    }
 }
