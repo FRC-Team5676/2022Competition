2f4cbadd44dde0e2efa1c57fe29d5a1939af395c
Eric Macy <eric_macy@nonik.com>
2022-03-12 12:06:25 -0500

    Refactor variable name and code cleanup

--- src/main/java/frc/robot/ArmRotate.java
+++ src/main/java/frc/robot/ArmRotate.java
@@ -5,49 +5,32 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class ArmRotate {
 
-    private static WPI_TalonSRX armRotate = new WPI_TalonSRX(24);
-    private static boolean IsLatched;
+    private static WPI_TalonSRX _armRotate = new WPI_TalonSRX(24);
+    private static boolean _isLatched;
 
     public static void Init() {
-        armRotate.configFactoryDefault();
-        Latch();
-    }
-
-    public static void Latch() {
-        if (!IsLatched) {
-            RotateDown(-0.2);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-            }
-            armRotate.stopMotor();
-            IsLatched = true;
-        }
-    }
-
-    public static void Unlatch() {
-        IsLatched = false;
+        _armRotate.configFactoryDefault();
     }
 
     public static void RotateUp(double speed) {
-        if (IsLatched)
-            armRotate.stopMotor();
+        if (_isLatched)
+            _armRotate.stopMotor();
         else {
             speed = Math.abs(speed);
-            armRotate.set(speed);
+            _armRotate.set(speed);
         }
     }
 
     public static void RotateDown(double speed) {
-        if (IsLatched)
-            armRotate.stopMotor();
+        if (_isLatched)
+            _armRotate.stopMotor();
         else {
             speed = Math.abs(speed);
-            armRotate.set(-speed);
+            _armRotate.set(-speed);
         }
     }
 
     public static void RotateStop() {
-        armRotate.stopMotor();
+        _armRotate.stopMotor();
     }
 }
